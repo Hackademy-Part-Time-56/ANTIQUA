98a86585b3198af27e60aab73de214ab2c8a2090
Roberto Strajeru <qtdguc@gmail.com>
2025-07-18 21:02:53 +0200

    fix con il login+relazioni

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     protected $fillable = [
         'name'
     ];
+
+    protected function articles(): HasMany   
+    {
+        return $this->hasMany(Article::class);
+    }
 }
