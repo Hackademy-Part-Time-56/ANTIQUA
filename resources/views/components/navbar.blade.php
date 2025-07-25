530dd4da910db0d88be111ef930a5242d3951d31
Roberto Strajeru <qtdguc@gmail.com>
2025-07-25 15:32:01 +0200

    fix index.blade.php&navbar e cambiato logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,10 +8,10 @@
         </div>
 
         <!-- Searchbar -->
-        <form class="d-flex ms-auto" role="search" action="{{ route('article.search') }}" method="GET" >
+        <form class="d-flex ms-auto" role="search" action="{{ route('article.search') }}" method="GET">
             <div class="input-group">
                 <input type="search" name="query" class="form-control" placeholder="Search" aria-label="search">
-                <button type="submit" class="input-group-text btn btn-outline-success" id="basic-addon2" >
+                <button type="submit" class="input-group-text btn btn-outline-success" id="basic-addon2">
                     Search
                 </button>
             </div>
@@ -20,10 +20,17 @@
 
         <!-- Navigation Actions -->
         <div class="navbar-actions">
+            <a href="{{ route('create.article') }}" class="btn-sell">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+                </svg>
+                Vendi
+            </a>
             @guest
                 <a href="{{ route('login') }}" class="nav-action-link">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                     </svg>
                     <span>Accedi</span>
                 </a>
@@ -34,33 +41,40 @@
                     <span>Registrati</span>
                 </a>
             @else
-                <div class="user-menu">
-                    <button class="user-menu-trigger">
-                        <img src="https://ui-avatars.com/api/?name={{ Auth::user()->name }}" alt="Avatar" class="user-avatar">
-                        <span class="user-name">{{ Auth::user()->name }}</span>
-                        <svg class="dropdown-arrow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            <div class="user-menu">
+                <button class="user-menu-trigger">
+                    <img src="https://ui-avatars.com/api/?name={{ Auth::user()->name }}" alt="Avatar"
+                        class="user-avatar">
+                    <span class="user-name">{{ Auth::user()->name }}</span>
+                    <svg class="dropdown-arrow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                    </svg>
+                </button>
+                <div class="user-dropdown">
+                    <div class="dropdown-divider"></div>
+                    <a href="#" class="dropdown-link"
+                        onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                         </svg>
-                    </button>
-                    <div class="user-dropdown">
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-link" onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                            </svg>
-                            Esci
-                        </a>
-                        <form action="{{route('logout')}}" method="post" class="d-none" id="form-logout">@csrf</form>
-                    </div>
+                        Esci
+                    </a>
+                    <form action="{{route('logout')}}" method="post" class="d-none" id="form-logout">@csrf</form>
+                    @auth
+                        @if (Auth::user()->is_revisor)
+                            <a class="nav-link btn btn-outline-success btn-sm position-relative w-sm-25"
+                                href="{{ route('revisor.index') }}">Zona Revisore
+                                <span
+                                    class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{{ \App\Models\Article::toBeRevisedCount() }}
+                                </span>
+                            </a>
+                        @endif
+                    @endauth
                 </div>
+            </div>
             @endauth
-
-            <a href="{{ route('create.article') }}" class="btn-sell">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-                </svg>
-                Vendi
-            </a>
         </div>
 
 
@@ -77,9 +91,12 @@
     <div class="navbar-secondary">
         <div class="navbar-containertwo">
             <nav class="secondary-nav">
-                <a href="{{ route('homepage') }}" class="secondary-link {{ request()->routeIs('homepage') ? 'active' : '' }}">Home</a>
-                <a href="{{ route('article.index') }}" class="secondary-link {{ request()->routeIs('article.index') ? 'active' : '' }}">Tutti gli articoli</a>
-                
+                <a href="{{ route('homepage') }}"
+                    class="secondary-link {{ request()->routeIs('homepage') ? 'active' : '' }}">Home</a>
+                <a href="{{ route('article.index') }}"
+                    class="secondary-link {{ request()->routeIs('article.index') ? 'active' : '' }}">Tutti gli
+                    articoli</a>
+
                 <div class="category-dropdown">
                     <button class="secondary-link category-trigger">
                         Categorie
@@ -90,10 +107,12 @@
                     <div class="category-mega-menu">
                         <div class="mega-menu-grid">
                             @foreach ($categories as $category)
-                                <a href="{{route('byCategory',['category'=>$category])}}" class="category-item">
+                                <a href="{{route('byCategory', ['category' => $category])}}" class="category-item">
                                     <div class="category-icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
                                         </svg>
                                     </div>
                                     <span>{{ $category->name }}</span>
@@ -102,24 +121,8 @@
                         </div>
                     </div>
                 </div>
-                
+
             </nav>
         </div>
     </div>
-
-<!-- Collegamento Area Del Revisore -->
-
-            @auth
-        @if (Auth::user()->is_revisor)
-        
-            <li class="nav-item">
-                        <a class="nav-link btn btn-outline-success btn-sm position-relative w-sm-25"
-                        href="{{ route('revisor.index') }}">Zona Revisore
-                        <span
-                        class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{{ \App\Models\Article::toBeRevisedCount() }}
-                        </span>
-                        </a>
-            </li>
-        @endif
-    @endauth        
 </nav>
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -4,7 +4,7 @@
             <div class="col-3">
                 <div class="rounded shadow bg-body-secondary">
                     <h1 class="display-5 text-center pb-2">Dashboard Revisore
-                    </h1> 
+                    </h1>
                 </div>
             </div>
         </div>
@@ -13,7 +13,7 @@
         <div class="row justify-content-center pt-5">
             <div class="col-md-8">
                 <div class="row justify-content-center">
-                    @for ($i = 0; $i <6; $i++)
+                    @for ($i = 0; $i < 6; $i++)
                         <div class="col-6 col-md-4 mb-4 text-center">
                             <img src="https://picsum.photos/300" class="img-fluid rounded shadow" alt="immagine segnaposto">
                         </div>
@@ -29,14 +29,20 @@
                     <p class="h6">{{ $article_to_check->description }}</p>
                 </div>
                 <div class="d-flex pb-4 justify-content-around">
-                    <form action="" method="POST">
-                        @csrf
-                        <button class="btn btn-danger py-2 px-5 fw-bold">Rifiuta</button>
-                    </form>
-                    <form action="" method="POST">
-                        @csrf
-                        <button class="btn btn-success py-2 px-5 fw-bold">Accetta</button>
-                    </form>
+                    <div class="d-flex pb-4 justify-content-around">
+                        <form action="{{ route('reject', ['article' => $article_to_check]) }}" method="POST">
+                            @csrf
+                            @method('PATCH')
+                            <button class="btn btn-danger py-2 px-5 fw-bold">Rifiuta</button>
+                        </form>
+
+                        <form action="{{ route('accept', ['article' => $article_to_check]) }}" method="POST">
+                            @csrf
+                            @method('PATCH')
+                            <button class="btn btn-success py-2 px-5 fw-bold">Accetta</button>
+                        </form>
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -45,6 +51,13 @@
             <div class="col-12">
                 <h1 class="fst-italic display-4">Non ci sono articoli da revisionare</h1>
                 <a href="{{ route('homepage') }}" class="mt-5 btn btn-success">Torna alla home</a>
+            </div>
+        </div>
+    @endif
+    @if (session()->has('message'))
+        <div class="row justify-content-center">
+            <div class="col-5 alert alert-success text-center shadow rounded">
+                {{ session('message') }}
             </div>
         </div>
     @endif
